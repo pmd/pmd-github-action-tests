cffafa8efaec7574f1d1721241a73617d8bb85ec
Andreas Dangel <andreas.dangel@pmd-code.org>
2021-11-26 20:42:09 +0100

    Initial commit

---
+++ src/main/java/AvoidCatchingThrowableSample.java
@@ -0,0 +1,9 @@
+public class AvoidCatchingThrowableSample {
+    public void bar() {
+        try {
+            // do something
+        } catch (Throwable th) {  // should not catch Throwable
+            th.printStackTrace();
+        }
+    }
+}
