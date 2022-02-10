986ab1c0a027dcb1b3266c7052fb659c88ddaf47
Andreas Dangel <andreas.dangel@pmd-code.org>
2022-02-10 14:06:32 +0100

    Revert "Test: Violations in new files"
    This reverts commit ece4efc86fa9ccaab95ad01d1fb732238313feeb.

--- src/main/java2/AbstractClassWithoutAnyMethodSample.java
+++
@@ -1,4 +0,0 @@
-public abstract class AbstractClassWithoutAnyMethodSample { // should be reported as "AbstractClassWithoutAbstractMethod" (prio 1, failure)
-    String field;
-    int otherField;
-}
