98f29694d44e58ece90b8f712525f6f30bcbdaf3
Andreas Dangel <andreas.dangel@pmd-code.org>
2022-06-04 09:59:29 +0200

    Revert "Test: Violations in new files"
    This reverts commit 7bf9dc16fb014d123631a977c1f0b4bb40b99caa.

--- src/main/java2/UnconditionalIfStatementSample.java
+++
@@ -1,7 +0,0 @@
-public class UnconditionalIfStatementSample {
-    public void close() {
-        if (true) {        // should be reported as "UnconditionalIfStatement" (prio 4, warning)
-            // ...
-        }
-    }
-}
