ca425e0ba6db115aa3fc632e473bc959274b1309
Andreas Dangel <andreas.dangel@pmd-code.org>
2024-03-17 18:27:24 +0100

    Test "Violations in new files"
    Refs pmd/pmd-github-action#239

---
+++ src/main/java2/AvoidCatchingThrowableSample.java
@@ -0,0 +1,9 @@
+public class AvoidCatchingThrowableSample {
+    public void bar() {
+        try {
+            // do something
+        } catch (Throwable th) {  // should be reported as "AvoidCatchingThrowable" (prio 2, failure)
+            th.printStackTrace();
+        }
+    }
+}
