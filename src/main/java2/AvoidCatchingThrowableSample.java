34fc92fd4621d19aa6e68632914e77d04c1c57e8
Andreas Dangel <andreas.dangel@pmd-code.org>
2024-03-17 18:40:04 +0100

    Revert "Test "Violations in new files"" [skip ci]
    This reverts commit ca425e0ba6db115aa3fc632e473bc959274b1309.

--- src/main/java2/AvoidCatchingThrowableSample.java
+++
@@ -1,9 +0,0 @@
-public class AvoidCatchingThrowableSample {
-    public void bar() {
-        try {
-            // do something
-        } catch (Throwable th) {  // should be reported as "AvoidCatchingThrowable" (prio 2, failure)
-            th.printStackTrace();
-        }
-    }
-}
